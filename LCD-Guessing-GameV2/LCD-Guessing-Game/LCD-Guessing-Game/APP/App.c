327cbdc72cc62d90c74497065e0eda34a16202f9
Omar Tarek <omar.t4352@gmail.com>
2021-09-18 00:05:56 +0200

    remove V1.0 codes

--- LCD-Guessing-GameV2/LCD-Guessing-Game/LCD-Guessing-Game/APP/App.c
+++ LCD-Guessing-GameV2/LCD-Guessing-Game/LCD-Guessing-Game/APP/App.c
@@ -19,21 +19,6 @@ void app_init()
 
 void get_user_press()
 {
-	/*if (GETBIT(TIFR,bit4) != compare_match_flag)
-	{
-		if(GETBIT(PINC, button))
-		{
-			delay_ms(300); // for debuncing
-			if(GETBIT(PINC, button))
-			{
-				user_number++;
-				go_to_row_column(1,7);
-				LCD_intgerToString(user_number);
-				config timer_conf = {timer1, compare, f_cpu_clk_1024, ticks};
-				Timer_init(&timer_conf);
-			}
-		}
-	}*/
 	 key = get_key();
 	if(key != enter)
 	{
@@ -45,47 +30,6 @@ void get_user_press()
 }
 void check_user_number()
 {
-	/*if(GETBIT(TIFR,bit4) == compare_match_flag)
-	{
-		if(user_number < min)
-		{
-			send_cmd(CLEAR_COMMAND); // LCD at the beginning 
-			go_to_row_column(0,0);
-			send_string("YOU DIDN'T");
-			go_to_row_column(1,0);
-			send_string("CHOOSE A NUMBER");
-		}
-		else if(user_number > max)
-		{
-			send_cmd(CLEAR_COMMAND); //clear LCD at the beginning 
-			go_to_row_column(0,0);
-			send_string("THE VALID");
-			go_to_row_column(1,0);
-			send_string("NUMB WITHIN 1:15");
-		}
-		else
-		{
-			if(user_number == random_num(min,max))
-			{
-				send_cmd(CLEAR_COMMAND); //clear LCD at the beginning 
-				go_to_row_column(0,0);
-				send_string("YOU WON");
-			}
-			else
-			{
-				send_cmd(CLEAR_COMMAND); //clear LCD at the beginning 
-				go_to_row_column(0,0);
-				send_string("YOU LOS");
-			}
-			
-		}
-		user_number = 0;
-		delay_ms(2000);
-		home_screen();
-		SETBIT(TIFR,bit4); // re-enable entering number
-	}*/
-	
-	
 	delay_ms(1000);
 	if(key == enter)
 	{
@@ -141,7 +85,4 @@ void app()
 {
 	get_user_press();
 	check_user_number();
-	/*go_to_row_column(1,7);
-	user_number = get_key();
-	LCD_intgerToString(user_number);*/
 }
